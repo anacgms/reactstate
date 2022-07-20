2fab94d14fd9da965bfb7b55420dcbfff7be25b3
anacgms <acgomes43@gmail.com>
2022-07-20 14:22:46 -0300

    class

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react';
+import "./App.css";
 
-function App() {
+class App extends Component{
+
+
+state = {
+        
+        nome: "Ana Carolina",
+        idade: 25,
+        comidaFavorita: "Bife com batata frita",
+        musicas: ["Oceano","smile", "correnteza"]
+   
+      
+}
+
+  render(){
     return(
-      <div>
-        <p>Oiee.</p>
-      </div>
-  )
-} 
+      <section>
+        <h1>Oi, me chamo {this.state.nome}</h1>
+        <h2>Tenho {this.state.idade} anos!</h2>
+        <h3>Gosto muito de comer {this.state.comidaFavorita}</h3>
+        <ol>
+          <li>{this.state.musicas[0]}</li>
+          <li>{this.state.musicas[1]}</li>
+          <li className='item'>{this.state.musicas[2]}</li>
+        </ol>
+    </section>
+     
+    )
+  }
+}
 
 export default App;
-
-
-
-  //state = {
-        //nome: "Ana Carolina",
-        //idade: 25,
-        //comidaFavorita: "Bife com batata frita",
-        //musicas: ["Oceano","smile", "correnteza"]
-   
-
-//}
-
-  //render(){
-    //return(
-      //<section>
-        //<h1>Oi, me chamo {this.state.nome}</h1>
-        //<h2>Tenho {this.state.idade} anos!</h2>
-        //<h3>Gosto muito de comer {this.state.comidaFavorita}</h3>
-        //<ol>
-          //<li>{this.state.musicas[0]}</li>
-          //<li>{this.state.musicas[1]}</li>
-          //<li className='item'>{this.state.musicas[2]}</li>
-        //</ol>
-        //<img src={melancia} alt="melancia"/> 
-      //</section>
-     
-    //)
- // }
-//}
-
-//export default App;
 
 
 
